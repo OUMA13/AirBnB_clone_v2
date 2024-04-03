f27405dc82b47b08c20804e1e70fff37b9debfb5
OUMA13 <omaimaelkhanchoufi9@gmail.com>
2024-04-03 16:41:02 -0700

    tas1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
-# Prepare web server for deployment
-apt-get -y update
+# a Bash script that sets up your web servers for the deployment of web_static
+
+apt-get update
 apt-get -y install nginx
-mkdir -p /data/web_static/shared/
+# Install Nginx if it not already installed
 mkdir -p /data/web_static/releases/test/
-echo "<html>
+# Create the folder /data/ if it doesn’t already exist
+mkdir -p /data/web_static/shared/
+# Create the folder /data/web_static/shared/ if it doesn’t already exist
+echo '<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
+</html>' > /data/web_static/releases/test/index.html
+# Create a fake HTML file /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-grep -q "location /hbnb_static" /etc/nginx/sites-available/default || \
-sed -i "/^}/i \ \n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-available/default
+# ...exists, it should be deleted and recreated every time the script is ran.
+chown -hR ubuntu:ubuntu /data/
+sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
 service nginx restart
