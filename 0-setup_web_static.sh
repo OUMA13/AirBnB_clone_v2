73b10ee61b3cb212aa238e2ea9fdadf697400ca2
OUMA13 <omaimaelkhanchoufi9@gmail.com>
2024-04-03 17:02:47 -0700

    tas1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-# a Bash script that sets up your web servers for the deployment of web_static
+# sets up web servers for the deployment of web_static 
 
 apt-get update
 apt-get -y install nginx
 # Install Nginx if it not already installed
 mkdir -p /data/web_static/releases/test/
-# Create the folder /data/ if it doesn’t already exist
 mkdir -p /data/web_static/shared/
 # Create the folder /data/web_static/shared/ if it doesn’t already exist
 echo '<html>
@@ -15,9 +14,7 @@ echo '<html>
     Holberton School
   </body>
 </html>' > /data/web_static/releases/test/index.html
-# Create a fake HTML file /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-# ...exists, it should be deleted and recreated every time the script is ran.
 chown -hR ubuntu:ubuntu /data/
 sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
 service nginx restart
